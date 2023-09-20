9b56160466121e865c06aef8bdc461c3a00424e6
AlvaroUrena <alvarourenacarrillo11@gmail.com>
2023-09-20 10:26:22 +0200

    primera practica

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('parrafo').innerHTML = 'Hola'
+function suma() {
+  console.log(2 + 2)
+  console.log(2 + 2)
+  console.log(2 + 2)
+  console.log(2 + 2)
+  console.log(2 + 2)
+  console.log(2 + 2)
+}
+suma()
+suma()
+suma()
+suma()
+
+document.getElementById('parrafo').style.fontSize = '2rem'
+document.getElementById('parrafo').style.color = 'red'
+document.getElementById('parrafo').style.backgroundColor = 'yellow'
+document.getElementById('imagen').src = '/img/asi_que_diselo.png'
