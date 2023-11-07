94544fc8a01d021ca2a32f3a6e9db087624f48f3
Alvaro Urena <alvaro.urena@educa.madrid.org>
2023-11-07 10:35:16 +0100

    empezando buscaminas

---
+++ buscaminas/iniciarBuscaminas.js
@@ -0,0 +1,23 @@
+// DOM
+const tabla = document.querySelector('.tabla')
+
+function generarTableroJS() {
+  // TODO: VALIDAR LOS DATOS
+  const ancho = prompt('Dame el ancho pls:')
+  const alto = prompt('Dame el alto pls:')
+
+  // Añadir css
+  tabla.style.cssText += `
+    grid-template-columns:repeat(${ancho}, 1fr);
+    grid-template-rows:repeat(${alto}, 1fr);
+  `
+
+  // Crear botones
+  for (let i = 0; i < ancho * alto; i++) {
+    const button = document.createElement('button')
+    button.textContent = 'x'
+    tabla.append(button)
+  }
+}
+
+generarTableroJS()
