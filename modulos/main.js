9d4433f72d1d3ea77337df7fff151eeb9d26d39b
Alvaro Urena <alvaro.urena@educa.madrid.org>
2024-02-08 12:19:14 +0100

    modulos export e import

---
+++ modulos/main.js
@@ -0,0 +1,5 @@
+import { variables, nombre } from './src/moduloNombrado.js'
+import funcionSumar from './src/moduloDefault.js'
+
+console.log(variables, nombre)
+console.log(funcionSumar(2, 2))
---
+++ modulos/src/moduloNombrado.js
@@ -0,0 +1,8 @@
+const variables = {
+  a: 2,
+  b: 2
+}
+
+const nombre = 'Alvaro'
+
+export { variables, nombre }
